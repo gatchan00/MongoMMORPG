b7854ca185a0ed47dfae10044c1e1f8ace73edd2
Javier Gomez Santos <javier.gomez.santos@everis.com>
2018-09-17 16:47:22 +0200

    cambios para enlazar a phaser

--- pasos/4-valida usuario y entra en juego/valida-usuario.js
+++ pasos/4-valida usuario y entra en juego/valida-usuario.js
@@ -1,7 +1,58 @@
 
 function createWebSocketValidaUsuario(user, pass, mensaje){
 var ws=new WebSocket("ws://127.0.0.1:8000");
-var currentTicket=null
+var currentTicket=null			
+
+let juegoPhaser=function(){
+	var config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 200 }
+            }
+        },
+        scene: {
+            preload: preload,
+            create: create
+        }
+    };
+
+    var game = new Phaser.Game(config);
+
+    function preload ()
+    {
+        //this.load.setBaseURL('http://labs.phaser.io');
+
+        this.load.image('sky', '../assets/space3.png');
+        this.load.image('logo', '../assets/phaser3-logo.png');
+        this.load.image('red', '../assets/red.png');
+    }
+
+    function create ()
+    {
+        this.add.image(400, 300, 'sky');
+
+        var particles = this.add.particles('red');
+
+        var emitter = particles.createEmitter({
+            speed: 100,
+            scale: { start: 1, end: 0 },
+            blendMode: 'ADD'
+        });
+
+        var logo = this.physics.add.image(400, 100, 'logo');
+
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+
+        emitter.startFollow(logo);
+    }
+}
+
 ws.onopen=function(){
 		var mensajeValidaUsuario = {};
 		mensajeValidaUsuario['operacion'] = 'valida_usuario';
@@ -16,7 +67,13 @@ ws.onmessage = function (evt)
 			console.log("recibo mensaje: ");
 			console.log(ws.receivedJson)
 			var preParsedJson=JSON.parse(ws.receivedJson);
-			document.getElementById(mensaje).innerHTML = ws.receivedJson
+
+			if (preParsedJson.error_code==0){
+				currentTicket=preParsedJson.ticket_id;
+				document.getElementById("idbody").innerHTML = null;
+				juegoPhaser();
+			}
+			document.getElementById(mensaje).innerHTML = currentTicket
 		}
 	};
 ws.onclose = function()
